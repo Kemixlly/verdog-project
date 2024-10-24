bc0e4a29332a4fad998061defaa1b35a79b6feed
Kemixlly <150630967+Kemixlly@users.noreply.github.com>
2024-10-24 10:47:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   // Create a map instance and set the initial view coordinates and zoom level
   var map = L.map('map').setView([40.75527789581924, 15.535535290147822], 13);
 
-
+  var greenIcon = L.icon({
+    iconUrl: 'https://t3.ftcdn.net/jpg/02/73/16/24/240_F_273162497_ShAAB1TH0vhM4UUWbhBuao8jtGBDubwD.jpg',
+ 
+    iconSize:     [80, 60], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+  
   // Add a tile layer to the map from OpenStreetMap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -12,5 +19,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Create a marker with popup and add it to the map
   var marker = L.marker([51.505, -0.09]).addTo(map);
-  marker.bindPopup("Hello, I'm a marker!").openPopup();
+  marker.bindPopup("Olá! Eu sou o Verdog!").openPopup();
+
+ var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
+  marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
+  
 });
