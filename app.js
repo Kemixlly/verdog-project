5e465913a72b1d61cc2e979a5ede04e488ba1081
Kemixlly <150630967+Kemixlly@users.noreply.github.com>
2024-10-24 10:52:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var map = L.map('map').setView([40.75527789581924, 15.535535290147822], 13);
 
   var greenIcon = L.icon({
-    iconUrl: 'https://t3.ftcdn.net/jpg/02/73/16/24/240_F_273162497_ShAAB1TH0vhM4UUWbhBuao8jtGBDubwD.jpg',
+    iconUrl: 'https://pt.quizur.com/_image?href=https://img.quizur.com/f/img638416f95c3a02.09155291.jpg?lastEdited=1669601029&w=600&h=600&f=webp.jpg',
  
     iconSize:     [80, 60], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var marker = L.marker([51.505, -0.09]).addTo(map);
   marker.bindPopup("Olá! Eu sou o Verdog!").openPopup();
 
- var marker2 = L.marker([-23.52026413037824, -46.83670586136634], {icon: greenIcon}).addTo(map);
-  marker2.bindPopup("Amada turma de DMD, eu me perdi aqui").openPopup();
+ var marker2 = L.marker([20.308067887461203, -74.29669802433997], {icon: greenIcon}).addTo(map);
+  marker2.bindPopup("De tu querida presencia... Comandante Che Guevara").openPopup();
   
 });
